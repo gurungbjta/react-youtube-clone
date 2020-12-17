dfbe4e45442a1e5baba48fe87a7740dd629dd4a1
gurungbjta <bijayata.gurung@sjsu.edu>
2020-12-16 20:08:41 -0800

    sidebar done

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import './Sidebar.css'
-import SidebarRow from "./SidebarRow";
 import HomeIcon from '@material-ui/icons/Home';
 import WhatshotIcon from '@material-ui/icons/Whatshot';
 import SubscriptionsIcon from '@material-ui/icons/Subscriptions';
+import VideoLibraryIcon from '@material-ui/icons/VideoLibrary';
+import HistoryIcon from '@material-ui/icons/History';
+import OndemandVideoIcon from '@material-ui/icons/OndemandVideo';
+import WatchLaterIcon from '@material-ui/icons/WatchLater';
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import SidebarRow from "./SidebarRow";
 
 function Sidebar() {
     return (
@@ -12,6 +18,13 @@ function Sidebar() {
             <SidebarRow Icon={WhatshotIcon} title="Trending" />
             <SidebarRow Icon={SubscriptionsIcon} title="Subscription" />
             <hr />
+            <SidebarRow Icon={VideoLibraryIcon} title="Library" />
+            <SidebarRow Icon={HistoryIcon} title="History" />
+            <SidebarRow Icon={OndemandVideoIcon} title="Your Videos" />
+            <SidebarRow Icon={WatchLaterIcon} title="Watch Later" />
+            <SidebarRow Icon={ThumbUpAltIcon} title="Liked Videos" />
+            <SidebarRow Icon={ExpandMoreIcon} title="Show More" />
+            <hr/>
         </div>
     )
 }
